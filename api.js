bfa476e44a5e95e8e3a0f2a1993277480021c866
adithyahk199 <adithyahk199@gmail.com>
2026-06-20 11:52:45 +0530

    updating api
    api

--- api.js
+++ api.js
@@ -1,1 +1,35 @@
 
+import { API_KEY } from "./config.js";
+import { getProxyUrl } from "./helpers.js";
+
+export async function fetchTopNews(category = "general") {
+
+    const apiUrl =
+        `https://gnews.io/api/v4/top-headlines?category=${category}&lang=en&country=in&max=20&apikey=${API_KEY}`;
+
+    const response = await fetch(getProxyUrl(apiUrl));
+
+    const data = await response.json();
+
+    if (!response.ok) {
+        throw new Error(data.message || "Failed to fetch news");
+    }
+
+    return data.articles;
+}
+
+export async function searchNews(query) {
+
+    const apiUrl =
+        `https://gnews.io/api/v4/search?q=${encodeURIComponent(query)}&lang=en&max=20&apikey=${API_KEY}`;
+
+    const response = await fetch(getProxyUrl(apiUrl));
+
+    const data = await response.json();
+
+    if (!response.ok) {
+        throw new Error(data.message || "Failed to search news");
+    }
+
+    return data.articles;
+}
