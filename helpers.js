fafa66ff3a194378e1deb024644d755850edbd2e
adithyahk199 <adithyahk199@gmail.com>
2026-06-20 11:50:45 +0530

    helpers
    corsproxy

--- helpers.js
+++ helpers.js
@@ -1,1 +1,11 @@
+export function showLoader(loader) {
+    loader.style.display = "block";
+}
 
+export function hideLoader(loader) {
+    loader.style.display = "none";
+}
+
+export function getProxyUrl(url) {
+    return `https://corsproxy.io/?${encodeURIComponent(url)}`;
+}
