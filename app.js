d9bab26adccbce807fc35d64c268d6ed7455dca0
adithyahk199 <adithyahk199@gmail.com>
2026-06-20 12:00:14 +0530

    app
    app final

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+import { setupEvents } from "./events.js";
 
+setupEvents({
+
+    loader: document.getElementById("loader"),
+    newsContainer: document.getElementById("newsContainer"),
+    errorDiv: document.getElementById("error"),
+    searchBtn: document.getElementById("searchBtn"),
+    searchInput: document.getElementById("searchInput"),
+    categorySelect: document.getElementById("category")
+
+});
