75a1c66443ea8a48da4b75a750d623c1b5fb7806
adithyahk199 <adithyahk199@gmail.com>
2026-06-20 11:54:34 +0530

    updating ui
    user interface

--- ui.js
+++ ui.js
@@ -1,1 +1,42 @@
+export function displayNews(newsContainer, articles) {
 
+    newsContainer.innerHTML = "";
+
+    if (!articles.length) {
+        newsContainer.innerHTML =
+            `<div class="no-news">No News Found</div>`;
+        return;
+    }
+
+    articles.forEach(article => {
+
+        const card = document.createElement("div");
+
+        card.className = "card";
+
+        card.innerHTML = `
+            <img src="${article.image || "https://via.placeholder.com/400x220"}">
+
+            <div class="card-content">
+
+                <small>
+                    ${article.source?.name || "Unknown Source"}
+                    •
+                    ${new Date(article.publishedAt).toLocaleDateString()}
+                </small>
+
+                <h3>${article.title}</h3>
+
+                <p>${article.description || ""}</p>
+
+                <a href="${article.url}" target="_blank">
+                    Read Full Article
+                </a>
+
+            </div>
+        `;
+
+        newsContainer.appendChild(card);
+    });
+
+}
